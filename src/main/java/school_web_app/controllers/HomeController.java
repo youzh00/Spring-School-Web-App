66ad06bc975f7ebd61d7e2f9c10a16674425f7a8
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-12-11 17:10:24 +0100

    Fist Hello world

---
+++ src/main/java/school_web_app/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package school_web_app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/home","/",""})
+    public  String home(){
+        return  "home.html";
+    }
+}
