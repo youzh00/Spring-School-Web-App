6caf2fff817adcca598f0c69d915d28d197e002d
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-12-11 18:01:11 +0100

    Adding Thymeleaf

--- src/main/java/school_web_app/controllers/HomeController.java
+++ src/main/java/school_web_app/controllers/HomeController.java
@@ -1,13 +1,15 @@
 package school_web_app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"/home","/",""})
-    public  String home(){
+    public  String home(Model model){
+        model.addAttribute("username","Youssef Zahi");
         return  "home.html";
     }
 }
