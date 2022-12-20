bff808375dd0509be78f098fd3081a23b97e1a32
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-12-20 18:05:40 +0100

    Submiting contact form method

--- src/main/java/school_web_app/controllers/ContactController.java
+++ src/main/java/school_web_app/controllers/ContactController.java
@@ -1,9 +1,15 @@
 package school_web_app.controllers;
 
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.logging.Logger;
 
 @Controller
 public class ContactController {
@@ -12,4 +18,12 @@ public class ContactController {
     public String displayContactPage(){
         return "contact.html";
     }
+
+    @PostMapping(value = "/saveMsg")
+    public ModelAndView saveMessage(@RequestParam String name,@RequestParam String mobileNum,
+            @RequestParam String email,@RequestParam String subject,@RequestParam String message){
+        System.out.println("Name :"+name);
+        return new ModelAndView("redirect:/contact");
+    }
+
 }
