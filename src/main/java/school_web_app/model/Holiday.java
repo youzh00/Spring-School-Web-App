effc8c14cefeb1bca42dbe8851582dfec3c5f6f4
Youssef Zahi <youssef.zahi2016@gmail.com>
2022-12-28 15:04:58 +0100

    Implementing lombok library

--- src/main/java/school_web_app/model/Holiday.java
+++ src/main/java/school_web_app/model/Holiday.java
@@ -1,29 +1,15 @@
 package school_web_app.model;
 
+import lombok.Data;
+import lombok.Getter;
+
+@Data
 public class Holiday {
+
     public final String day;
     public final String reason;
     public final Type type;
-
     public enum Type{
         FESTIVAL, FEDERAL
-    }
-
-    public Holiday(String day, String reason, Type type) {
-        this.day = day;
-        this.reason = reason;
-        this.type = type;
-    }
-
-    public String getDay() {
-        return day;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public Type getType() {
-        return type;
     }
 }
--- src/main/java/school_web_app/service/ContactService.java
+++ src/main/java/school_web_app/service/ContactService.java
@@ -1,14 +1,17 @@
 package school_web_app.service;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import school_web_app.model.Contact;
 
+@Slf4j
 @Service
 public class ContactService {
 
     public boolean submitContactFormDetail(Contact contact){
         boolean isSaved=true;
+
         //TODO- persist data to the DB
         System.out.println(contact.toString());
         return isSaved;
