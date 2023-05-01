2e2a7717088233014812342c7da1c9031ed108ff
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-05-01 17:44:34 +0100

    create address and roles repos

--- src/main/java/school_web_app/repository/AddressRepository.java
+++ src/main/java/school_web_app/repository/AddressRepository.java
@@ -1,7 +1,10 @@
 package school_web_app.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school_web_app.model.Address;
 
+
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
--- src/main/java/school_web_app/repository/RolesRepository.java
+++ src/main/java/school_web_app/repository/RolesRepository.java
@@ -2,7 +2,9 @@ package school_web_app.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import school_web_app.model.Roles;
 
+@Repository
 public interface RolesRepository extends JpaRepository<Roles,Integer> {
 }
