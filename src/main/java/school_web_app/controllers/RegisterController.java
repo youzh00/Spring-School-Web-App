90e3d63dacf22ac3518964758fbf5fbc56fe98a7
Youssef Zahi <youssef.zahi2016@gmail.com>
2023-05-01 15:33:59 +0100

    Integrate annotations validation

--- src/main/java/school_web_app/controllers/RegisterController.java
+++ src/main/java/school_web_app/controllers/RegisterController.java
@@ -1,9 +1,13 @@
 package school_web_app.controllers;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import school_web_app.model.Person;
 
@@ -16,5 +20,14 @@ public class RegisterController {
     public String displayRegisterPage(Model model) {
         model.addAttribute("person", new Person());
         return "register.html";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(@Valid @ModelAttribute("person") Person person, Errors errors) {
+        if(errors.hasErrors()){
+            return "register.html";
+        }
+            return "redirect:/login?register=true";
+
     }
 }
--- src/main/java/school_web_app/model/Person.java
+++ src/main/java/school_web_app/model/Person.java
@@ -1,10 +1,60 @@
 package school_web_app.model;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import school_web_app.annotations.FieldsValueMatch;
+import school_web_app.annotations.PasswordValidator;
 
 @Data
+@Entity
+@FieldsValueMatch.List({
+        @FieldsValueMatch(
+                field = "pwd",
+                fieldMatch = "confirmPwd",
+                message = "Passwords do not match!"
+        ),
+        @FieldsValueMatch(
+                field = "email",
+                fieldMatch = "confirmEmail",
+                message = "Email addresses do not match!"
+        )
+})
 public class Person {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private int personId;
 
+    @NotBlank(message="Name must not be blank")
+    @Size(min=3, message="Name must be at least 3 characters long")
+    private String name;
+
+    @NotBlank(message="Mobile number must not be blank")
+    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+    private String mobileNumber;
+
+    @NotBlank(message="Email must not be blank")
+    @Email(message = "Please provide a valid email address" )
+    private String email;
+
+    @NotBlank(message="Confirm Email must not be blank")
+    @Email(message = "Please provide a valid confirm email address" )
+    @Transient
+    private String confirmEmail;
+
+    @NotBlank(message="Password must not be blank")
+    @Size(min=5, message="Password must be at least 5 characters long")
+    @PasswordValidator
+    private String pwd;
+
+    @NotBlank(message="Confirm Password must not be blank")
+    @Size(min=5, message="Confirm Password must be at least 5 characters long")
+    @Transient
+    private String confirmPwd;
 }
